fd694a1e583d97560458c3210a190b3f5f4de130
Wasif Khan <waasif.khaan@gmail.com>
2018-04-13 21:00:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
   object[key]=value;
   return object
-
+}
+function deleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object
+  
 }
