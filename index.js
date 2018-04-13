d60b613384c8c5df00b61e7e91947df81e123757
Wasif Khan <waasif.khaan@gmail.com>
2018-04-13 20:58:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var recipes = new Object();
 function updateObjectWithKeyAndValue(object, key, value){
-  return Object.assign(object,{[key]:value})
+  return Object.assign({}, object, { [key]: value })
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
   object[key]=value;
   return object
+
 }
