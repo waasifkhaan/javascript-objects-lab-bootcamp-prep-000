a2dab79f4cb184d923cc365ae4004ee524651119
Wasif Khan <waasif.khaan@gmail.com>
2018-04-13 20:55:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
-  recipes[key]='value';
+  object[key]=value;
   return recipes
 }
